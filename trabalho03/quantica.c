097ccda5fe2ca6c18fc6da0355b5b474d6ba6141
romuluxs <romuluxs@gmail.com>
2016-11-11 00:35:01 -0200

    Adicionando as tabelas para executar o programa

--- trabalho03/quantica.c
+++ trabalho03/quantica.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BASE_SIZE 3 // o tamanho do vetor com as bases.
+
+char base[BASE_SIZE] = {'+', 'x', 'o'};
+char table[BASE_SIZE][2] = {{'^', '>'}, 
+							{'/', '\\'}, 
+							{'@', 'G'}};
+
+// devolve o indice do elemento da base
+int base_index(char c) {
+	int i;
+	for (i = 0; i < BASE_SIZE; i++)	{
+		if(base[i] == c) return i;
+	}
+	return -1; // não achou
+}
+
+
 int main() {
+
+	int s, n;
+	scanf("%d %d", &s, &n);	
+	
 
 	return 0;
 }
