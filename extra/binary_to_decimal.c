c06efeef1bd070670163e02b17c0588fba9e5a1c
romuluxs <romuluxs@gmail.com>
2016-11-12 12:58:45 -0200

    Conversor de binario para decimal completo

--- extra/binary_to_decimal.c
+++ extra/binary_to_decimal.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 long long binary_to_decimal(int* bits, int bits_length) {
 	int i;
-	for(i = bits_length-1; i >= 0; i--) {
-		
+	long long decimal_value = 0;
+	for(i = 0; i < bits_length; i++) {
+		int exponent = bits_length - i - 1;
+		decimal_value += bits[i]*pow(2, exponent);
 	}
+	return decimal_value;
 }
 
-int main() {	
+int main() {
+	int n = 5;
+
+	int bits[5] = {0, 1, 1, 0, 1}; 
+
+	printf("Resultado final = %lli\n", binary_to_decimal(bits, n));
 
 }
+
+
 
