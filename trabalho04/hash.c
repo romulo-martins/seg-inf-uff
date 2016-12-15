4d11f773722994c7e9f2f1ec4d6960e07aad6b9d
Romuluxs <romuluxs@gmail.com>
2016-12-15 18:43:11 -0200

    Realizando a leitura de bits

--- trabalho04/hash.c
+++ trabalho04/hash.c
@@ -2,13 +2,34 @@
 #include <stdlib.h>
 #include <math.h>
 
+void read_bits(int* bits, int n) {
+	int i;
+	for (i = 0; i < n; i++) {
+		scanf("%d ", &bits[i]);
+	}
+	scanf("\n");	
+}
+
+void print_bits(int* bits, int n) {
+	int i;
+	for (i = 0; i < n; i++) {
+		printf("%d ", bits[i]);
+	}
+	printf("\n");
+}
+
 
 int main() {
 	
-	int k, d;
-	scanf("%d %d");
+	int k, d; // k bits de paridade
+	scanf("%d %d\n", &k, &d);
 
-	int n = pow(2, k) - 1 - k;
+	int n = pow(2, k) - 1 - k; // onde n é o tamanho dos bits
 
-	
+	int* bits = (int*)malloc(n*sizeof(int));
+	read_bits(bits, n);
+
+	print_bits(bits, n);
+
+	free(bits);
 }
